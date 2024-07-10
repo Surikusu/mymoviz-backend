0c7a8f6a14a74716cfa5c23d2ab253a651332a92
Robinson <robyvuillemin@gmail.com>
2024-07-10 12:23:55 +0200

    filter

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,22 @@ router.get('/movies', (req,res) => {
     fetch('https://api.themoviedb.org/3/discover/movie', options)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-        res.json(data)
+
+        const filtered = data.result.map(movie => ({
+            title: movie.title,
+            overview: movie.overview,
+            poster_path: movie.poster_path,
+            vote_average: movie.vote_average,
+            vote_count: movie.vote_count
+        }));
+
+        res.json(filtered)
     })
+
     .catch(err => console.error(err))
 })
+
+
+
 
 module.exports = router;
